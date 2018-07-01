4f2a68dcf5dd12752fccb4c678e223f48ef797d6
Lucas Everett <lucas@lucaseverett.codes>
2018-07-01 16:55:18 -0500

    Toolbar-Dial-Chrome

--- src/bookmarks.js
+++ src/bookmarks.js
@@ -1,7 +1,7 @@
 export default () =>
   new Promise(resolve =>
     chrome.bookmarks.getChildren("1", b => {
-      let bookmarks = b.map(({ title, url }) => {
+      let bookmarks = b.filter(({ url = "" }) => url).map(({ title, url }) => {
         return { title, url };
       });
       resolve(bookmarks);
